739de4bb3e3c5ec9525d20244236bb561a5d5661
educfn <eduardo.cardoso10@gmail.com>
2018-05-27 14:19:03 -0300

    2.6.5 - GLLb - Cr. Reptir e Desalocar
    Criação de área do codigo main dedicada a desaloção de memoria utilizada e Repitação do programa, caso o usuário deseje colocar outra linguagem.

--- Gramatica_Livre_de_Lambda/GLLambda/main.cpp
+++ Gramatica_Livre_de_Lambda/GLLambda/main.cpp
@@ -7,56 +7,74 @@ int main()
 {
     ling_f f;
     int var_controle=0;
+    char * esq;
+    char * dir;
 
-    //Coleta dos valores dos subconjuntos
-    std::cout << "Digite a seguir os valores dos subconjuntos, quando terminar digite somente"
-    << " 'y' na coleta dos valores." << std::endl;
-    std::cout << "A letra 'Y'(ipsilon maiusculo) indica lambda." << std::endl;
-
-    do
+    while(var_controle != 1)
     {
-        char * esq;
-        char * dir;
+          //Coleta dos valores dos subconjuntos
+        std::cout << "Digite a seguir os valores dos subconjuntos, quando terminar digite somente"
+        << " 'y' na coleta dos valores." << std::endl;
+        std::cout << "A letra 'Y'(ipsilon maiusculo) indica lambda." << std::endl;
 
-        //Alocacao dinamica das variaveis
-        if((esq = (char *) malloc(sizeof(char)*10))==nullptr)
+        do
         {
-            std::cerr << "Main - esq: Erro de alocacao dinamica!" << std::endl;
+            //Alocacao dinamica das variaveis
+            if((esq = (char *) malloc(sizeof(char)*10))==nullptr)
+            {
+                std::cerr << "Main - esq: Erro de alocacao dinamica!" << std::endl;
+            }
+
+            if((dir = (char *) malloc(sizeof(char)*10))==nullptr)
+            {
+                std::cerr << "Main - esq: Erro de alocacao dinamica!" << std::endl;
+            }
+
+            //Coleta dos valores
+            std::cout << "Digite as letra a esquerda do subconjunto(limite 10 letras): ";
+
+            std::cin >> esq;
+
+            std::cout << std::endl; //Pular uma linha.
+
+            std::cout << "Digite as letras a direita do subconjunto(limite 10 letras): ";
+
+            std::cin >> dir;
+
+            std::cout << std::endl;//Pular uma linha.
+
+            //Coletar e mostrar os valores e perguntar se necessita escrever mais subconjuntos.
+            inicializar_subcon(&f);
+
+            if(esq[0] != 'y' && guardar_subconj(&f,esq,dir) == 0)
+            {
+                std::cout << "Subconjunto (" << esq <<","<< dir << ") coletado!" << std::endl;
+                std::cout << "Quando terminar digite somente 'y' na coleta dos valores" << std::endl;
+                std::cout << "A letra 'Y'(ipsilon maiusculo) indica lambda." << std::endl;
+            }
+
+        } while(esq[0] != 'y');
+
+        //Mostrar os subconjuntos coletados
+
+        mostrar_linguagem(&f);
+
+        //Desalocar memoria
+        while(f.subconjuntos != nullptr)
+        {
+            free(f.subconjuntos->esq);
+            free(f.subconjuntos->dir);
+            conj aux = f.subconjuntos;
+            f.subconjuntos = f.subconjuntos->prox;
+            free(aux);
         }
 
-        if((dir = (char *) malloc(sizeof(char)*10))==nullptr)
-        {
-            std::cerr << "Main - esq: Erro de alocacao dinamica!" << std::endl;
-        }
-
-        //Coleta dos valores
-        std::cout << "Digite as letra a esquerda do subconjunto(limite 10 letras): ";
-
-        std::cin >> esq;
-
-        std::cout << std::endl; //Pular uma linha.
-
-        std::cout << "Digite as letras a direita do subconjunto(limite 10 letras): ";
-
-        std::cin >> dir;
-
-        std::cout << std::endl;//Pular uma linha.
-
-        //Coletar e mostrar os valores e perguntar se necessita escrever mais subconjuntos.
-        inicializar_subcon(&f);
-
-        if(esq[0] != 'y' && guardar_subconj(&f,esq,dir) == 0)
-        {
-            std::cout << "Subconjunto (" << esq <<","<< dir << ") coletado!" << std::endl;
-            std::cout << "Quando terminar digite somente 'y' na coleta dos valores" << std::endl;
-            std::cout << "A letra 'Y'(ipsilon maiusculo) indica lambda." << std::endl;
-        }else if(esq[0] == 'y') var_controle = 1;
-
-    } while(var_controle != 1);
-
-    //Mostrar os valores coletados
-
-    mostrar_linguagem(&f);
+        //Pergunta ao usuario se deseja colocar outra linguagem.
+        std::cout << "Deseja colocar uma nova linguagem?(S-sim,N-Nao)"<< std::endl;
+        char resposta[1];
+        std::cin >> resposta;
+        if(resposta[0] == 'N' || resposta[0] == 'n') var_controle = 1;
+    }
 
     return 0;
 }
